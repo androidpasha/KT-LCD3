510e6646d375d47fe446f7d25efd835ad4fb238e
Pavel Avramenko <androidpasha@gmail.com>
2023-12-18 19:01:50 +0200

    Готовий до дипломування

--- src/src.cpp
+++ src/src.cpp
@@ -2,37 +2,37 @@
 #pragma region offsetof //includes libs
 #include <Arduino.h>
 
-// библиотеки собственной разработки:
+// бібліотеки власної розробки:
 #include "KTLCD_Display.h"
 #include "AverageValue.h"
 #include "TimeBasedCounter.h"
 #include "SaveTemplateDataToLFS.h"
 #include "struct.h"
-#include "webSocketFunction.h" // функции веб сокета вынесены в отдельный файл
-// конец списка библиотек собственной разработки
+#include "webSocketFunction.h" 
+// кінець списку
 
 #include "Ticker.h"
-#include <ESP8266WiFi.h> // Библиотека для создания Wi-Fi подключения (клиент или точка доступа)
+#include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h>
-#include <FS.h> // Библиотека для работы с файловой системой
+#include <FS.h>
 #include <LittleFS.h>
 #pragma endregion
 #pragma region offsetof //constants
 #define MET_CALCULATION_CALORIES // підрахунок калорій через MET еквівалент коли розкоментований рядок або попередньою формулою, якщо закоментувати
-// #define SERIAL1                  // Закомментировать для ESP01 и если будет использоваться обычный Serial (0)
+#define SERIAL1                  // Use serial1
 #define MINIMUM_TIME_BETWEEN_WRITES_TO_THE_FILE_SYSTEM_SEC 60
-const char *ssidAP = "Bicycle";           // Название генерируемой точки доступа
+const char *ssidAP = "Bicycle";           // Назва точки доступу
 const float passPercent[3][6] PROGMEM = { // таблица відсотків допомоги PAS згідно параметру С14
                                           // {0, 6, 17, 30, 45, 80},  масив у відсотках з інструкції
                                           // {0, 12, 22, 40, 55, 80},
                                           // {0, 17, 30, 45, 66, 80}};
 
-    {1, 0.94, 0.83, 0.70, 0.55, 0.20}, // масив відсотків переведений у коефіцієнти за формулою
-    {1, 0.88, 0.78, 0.60, 0.45, 0.20}, // 100 - passPercent[pasAdjustment - 1][pasLevel]) / 100.0
-    {1, 0.83, 0.70, 0.55, 0.34, 0.20}};
+    {1.0f, 0.94f, 0.83f, 0.70f, 0.55f, 0.20f}, // масив відсотків переведений у коефіцієнти за формулою
+    {1.0f, 0.88f, 0.78f, 0.60f, 0.45f, 0.20f}, // 100 - passPercent[pasAdjustment - 1][pasLevel]) / 100.0
+    {1.0f, 0.83f, 0.70f, 0.55f, 0.34f, 0.20f}};
 
 const char *dataStorageFileNameControllerSettings PROGMEM = "/controllerSettings.bin";
 const char *dataStorageFileNameOdometr PROGMEM = "/measurementData.bin";
@@ -47,7 +47,7 @@ TimeBasedCounter wattMeter;
 TimeBasedCounter calories;
 AverageValue averageSpeed;
 Ticker scheduler_100ms;
-DNSServer dnsServer; // DNS сервер для перехода на главную страницу с http://bicycle/
+DNSServer dnsServer; // DNS для переходe з http://bicycle/
 AsyncWebServer webServer(80);
 AsyncWebSocket webSocket("/ws");
 SaveTemplateDataToLFS<SettingsStruct> SaveControllerDataToLFS(bicycle.settings, dataStorageFileNameControllerSettings);
@@ -69,36 +69,30 @@ void setup()
 {
   Serial.begin(9600); // Швидкість лінії контроллеру 9600 бод
 #ifdef SERIAL1
-  Serial.swap(); // Переключаем аппаратный serial c gpio3, gpio1 на gpio13, gpio15
+  Serial.swap(); // перемикаємо serial з gpio3, gpio1 на gpio13, gpio15
 #endif
-  //  LittleFS.begin(); // Инициализируем работу с файловой системой/ это сделано в SaveTemplateDataToLFS
-
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssidAP, "", 1, 0, 1);                              // (SSID, NOT PASS, Channel, no hidden ssid, max conenction)
-  dnsServer.start(53, "bicycle", WiFi.softAPIP());               // Переходим с любой страницы на IP точки доступа ESP8266
+  dnsServer.start(53, "bicycle", WiFi.softAPIP());               
   webServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request) // откр index.html при запросе корня сервера
                { request->send(LittleFS, "/index.html", "text/html"); });
   // відправляємо файли на HTTP запити з ФС
   webServer.serveStatic("/", LittleFS, "/");
-  // Инициализируем Web-сервер
   webServer.begin();
   webServer.addHandler(&webSocket);
-  // Функция обработки запросов
   webSocket.onEvent(webSocketEvent);
-  // Устанавливаем начальное значение счетчиков
+  // Початкові значення лічильників
   odometr.reset(measurementData.odoGeneral);
   wattMeter.reset(measurementData.wattMeter);
   calories.reset(measurementData.caloriesTotal);
-  // обнуляем счетчики поездки после включения
   measurementData.odoDrive = measurementData.odoGeneral;
   measurementData.caloriesDrive = measurementData.caloriesTotal;
-  // Вызываем функцию every100ms каждые 100 мс.
   scheduler_100ms.attach_ms(100, every100ms);
 }
 
 void loop()
 {
-  dnsServer.processNextRequest(); // Переходим с любой страницы на IP точки доступа ESP8266
+  dnsServer.processNextRequest();
 
   if (scheduleFlag == true)
   {
@@ -156,10 +150,10 @@ float calculateCalories(const User &user, float velocity, uint8_t pasLevel, uint
 {
 #ifdef MET_CALCULATION_CALORIES
   static const float BMR = // базові витрати калорій людиною у спокої за добу розраховуються один раз
-      (10.0 * user.weight + 6.25 * user.height - 5.0 * user.age + 5.0 * !user.sex - 161.0 * user.sex);
-  float МЕТ = 0.0086 * velocity * velocity + 0.1167 * velocity + 1.95;                     // Метаболічний еквівалент:
-  float DVK = BMR * (МЕТ - 1) * pgm_read_float(&passPercent[pasAdjustment - 1][pasLevel]); // додаткові витрати калорій людиною під час рівномірного фізичного навантаження (велосипед) без БВК з урахуванням допомоги PAS, за добу
-  return (BMR + DVK) * userParameters.calCorrectFactor / 24;                                                                 // за годину
+      (10.0f * user.weight + 6.25f * user.height - 5.0f * user.age + 5.0f * !user.sex - 161.0f * user.sex);
+  float МЕТ = 0.0086f * velocity * velocity + 0.1167f * velocity + 1.95f;                     // Метаболічний еквівалент:
+  float DVK = BMR * (МЕТ - 1.0f) * pgm_read_float(&passPercent[pasAdjustment - 1][pasLevel]); // додаткові витрати калорій людиною під час рівномірного фізичного навантаження (велосипед) без БВК з урахуванням допомоги PAS, за добу
+  return (BMR + DVK) * userParameters.calCorrectFactor / 24.0f;                                                                 // за годину
 #else
   // Стара формула E = BMR * 0.34 * velocity + 1000.0, ккал/добу
   static const float BMR = (10.0 * user.weight + 6.25 * user.height - 5.0 * user.age + 5.0 * !user.sex - 161.0 * user.sex); // базові витрати калорій людиною у спокої за добу
